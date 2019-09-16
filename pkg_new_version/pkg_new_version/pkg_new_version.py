d68c61c4c8dd098bee8c68909dc9a58d1c198440
Harald von Waldow <harald.vonwaldow@eawag.ch>
2019-09-17 00:12:14 +0200

    TODO text, superlous imports

--- pkg_new_version/pkg_new_version/pkg_new_version.py
+++ pkg_new_version/pkg_new_version/pkg_new_version.py
@@ -8,13 +8,17 @@ Usage:
 Arguments:
     <old_pkg_url>   URL of previous version of the data package
 
+TODO: 
+   + The newly introduces extras are not peristent when package is changed
+     via the UI. Need to put them into schmeming.
+   + "immutable" needs to be implemented
+   + GUI representation of everything
+
 '''
 import ckanapi
 import docopt
 import os
 import re
-import urllib
-import sys
 
 APIKEY = os.environ['CKAN_APIKEY_PROD1']
 HOSTPAT = re.compile(r'^(https?://[^/]+\.[^/]+).*$')
@@ -55,32 +59,49 @@ def _modify_pkg(pkg):
         oldversion = 1
         newversion = 2
         
+    # Set root_package_id field (if not yet present)
+    extras = pkg.get('extras', [])
+    for e in extras:
+        if e['key'] == 'root_pkg_id':
+            break
+    else:
+        # this is a first time versioning
+        extras.append({'key': 'root_pkg_id',
+                       'value': pkg['id']})
+    pkg['extras'] = extras
+    # Make way for new id
     del pkg['id']
-    pkg['title'] = '[Version {}] '.format(str(newversion)) + pkg['title']
+    # Strip previous version from title/name if present and add new version
+    title = re.sub(r'\[Version \d+\] ', '', pkg['title'])
+    pkg['title'] = '[Version {}] '.format(str(newversion)) + title
     name = pkg['title'].replace(' ', '_').lower()
     name = _safename(name)
     pkg['name'] = name[:99] if len(name) > 99 else name
+    # Set new version field, private and status, review_level
     pkg['version'] = newversion
+    pkg['private'] = True
+    pkg['status'] = 'incomplete'
+    pkg['review_level'] = 'none'
+    pkg['reviewed_by'] = ''
+    
+    # Clean resources of ids and set url_type indicating link, not upload
     for n, r in enumerate(pkg.get('resources', [])): 
         del pkg['resources'][n]['id']
         del pkg['resources'][n]['revision_id']
         pkg['resources'][n]['url_type'] = None
 
-    return oldversion
+    return extras
     
 def _create_new_pkg(pkg, conn):
-    print(pkg['name'])
     try:
         res = conn.call_action('package_create', data_dict=pkg)
     except Exception as e:
         print("\nERROR: Something went wrong creating the new package\n")
         raise e
-    print(res)
 
-def _update_old_pkg_version(conn, oldid, oldversion):
-    conn.call_action(
-        'package_patch',
-        data_dict={'id': oldid, 'version': oldversion})
+def _update_old_pkg_version(conn, oldpkgpatch):
+    conn.call_action('package_patch', oldpkgpatch)
+
         
 def main():
     args = docopt.docopt(__doc__)
@@ -88,9 +109,13 @@ def main():
     conn = _getconn(url)
     pkg = _read_pkg(url, conn)
     oldid = pkg['id']
-    oldversion = _modify_pkg(pkg)
+    oldversion = pkg.get('version') or 1
+    oldextras = _modify_pkg(pkg)
     _create_new_pkg(pkg, conn)
-    _update_old_pkg_version(conn, oldid, oldversion)
+    oldextras.append({'key': 'immutable', 'value': True})
+    oldpkgpatch = {'id': oldid, 'version': oldversion,
+                   'extras': oldextras}
+    _update_old_pkg_version(conn, oldpkgpatch)
 
 if __name__ == '__main__':
     main()
